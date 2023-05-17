61de5f2d3acec1d67365d8ce63b834794ee83ef4
Jmmk <jmk525@gmail.com>
2023-05-17 15:56:38 +0900

    도커 연결중

--- springboot/src/main/java/com/jojoldu/book/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/jojoldu/book/springboot/SpringbootApplication.java
@@ -3,7 +3,6 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //EnableJpaAuditing : JPA Auditing 활성화 어노테이션
 //@EnableJpaAuditing
