49115964e12dc3eb90795455ca4c5dc624effbf9
aristo <dkfud2121@naver.com>
2023-06-07 15:47:47 +0900

    feat : 게임 판정 알고리즘 구현

--- springboot/src/main/java/com/jojoldu/book/springboot/service/NumBall/NumBallService.java
+++ springboot/src/main/java/com/jojoldu/book/springboot/service/NumBall/NumBallService.java
@@ -21,4 +21,62 @@ public class NumBallService {
         return numballsRepository.save(requestDto.toEntity()).getId();
     }
 
+
+    //게임 진행 메서드
+
+    //임의 결과
+    static String answer="1234";
+    public static boolean getNumball(String guess){
+        if(answer.equals(guess)){
+            //결과 맞춤. end
+            System.out.println(" you are right! ");
+            return true;
+        } else{
+            //game 다시 하기
+            System.out.println(" Try again! ");
+            System.out.println(getResult(answer, guess));
+            return false;
+        }
+    }
+
+    // 입력값 결과 판정
+    public static String getResult(String answer, String guess){
+        int strikeNum=0;
+        int ballNum=0;
+
+        String[] answerArray = answer.split("");
+        String[] guessArray = guess.split("");
+        for(int i=0;i<answerArray.length;i++){
+            // 스트라이크 판정
+            if(answerArray[i].equals(guessArray[i])){
+                strikeNum++;
+                continue;
+            }
+            for(int j=0; j<answerArray.length;j++){
+                if(i!=j){
+                    // 볼 판정
+                    if(answerArray[i].equals(guessArray[j])){
+                        ballNum++;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return "result >> strike: "+strikeNum+" ball: "+ballNum;
+    }
+
+    public static void RunGame(){
+        Scanner scan = new Scanner(System.in);
+        String guess ;
+        while(true){
+            // 게임 진행. 입력값 받기
+            System.out.println("입력값 >> ");
+            guess = scan.nextLine();
+
+            if(getNumball(guess)){
+                break;
+            }
+        }
+    }
 }
