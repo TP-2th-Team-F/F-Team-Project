77521434e8b02c6dc4c7175670e16c9dccc3454b
Jmmk <jmk525@gmail.com>
2023-06-21 12:51:34 +0900

    docker yml 파일 수정 및 session table 생성

--- springboot/src/main/java/com/jojoldu/book/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/jojoldu/book/springboot/SpringbootApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
-
 }
